b26f61fb0b1325e4c7154145ede73669d425cb7a
Agnes Jardeleza <mabjardeleza@gmail.com>
2020-03-21 21:29:02 +1030

    feat: add document head title and description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Routing from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const App = () => (
-    <BrowserRouter>
-        <Routing />
-    </BrowserRouter>
+    <div>
+        <Helmet>
+            <title>CryptoCalc</title>
+            <meta name="description" content="A simple calculator for a portfolio of Cryptocurrency exchanges (Bitcoin, Litecoin and Ethereum), converting to a total in AUD" />
+        </Helmet>
+        <BrowserRouter>
+            <Routing />
+        </BrowserRouter>
+    </div>
 );
 
 export default App;

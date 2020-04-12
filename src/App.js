9dbd8e9eeafbc2a09f64e28f0c28f3dc146e3f6a
Agnes Jardeleza <mabjardeleza@gmail.com>
2020-04-12 15:09:16 +0930

    refactor: remove react-helmet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import Routing from './routes';
 import { BrowserRouter } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 
 const App = () => (
-    <div>
-        <Helmet>
-            <title>CryptoCalc</title>
-            <meta name="description" content="A simple calculator for a portfolio of Cryptocurrency exchanges (Bitcoin, Litecoin and Ethereum), converting to a total in AUD" />
-        </Helmet>
-        <BrowserRouter>
-            <Routing />
-        </BrowserRouter>
-    </div>
+  <BrowserRouter>
+    <Routing />
+  </BrowserRouter>
 );
 
 export default App;

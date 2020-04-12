c8e8e936f4eca8636c8d8ebaae38b749e1e16f63
Agnes Jardeleza <mabjardeleza@gmail.com>
2020-04-12 14:03:05 +0930

    feat: add store provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
+import { Provider } from 'mobx-react';
 import getStore from './getStore';
-
+import cryptoPricesStore from './stores/cryptoPricesStore';
+import currencyRatesStore from './stores/currencyRatesStore';
+import calculatorStore from './stores/calculatorStore';
 import App from './App';
 
-const store = getStore();
+const stores = {
+  cryptoPricesStore,
+  currencyRatesStore,
+  calculatorStore,
+};
 
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  ),
-  document.getElementById('root')
+  <Provider {...stores}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
